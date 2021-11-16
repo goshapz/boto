4c2411b3e8ef75cbdb22ed368f10c17597205807
gosha <gosha1253@mail.ru>
2021-11-16 17:18:08 +0000

    good

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ bot: TeleBot = telebot.TeleBot(token)
 def start(message):
     print(message.chat.id)
     keyboard = telebot.types.ReplyKeyboardMarkup()
-    keyboard.row("Сейчас", "Сегодня")
+    keyboard.row("По дате", "Сегодня")
     keyboard.row("Завтра", "Вчера")
     bot.send_message(message.chat.id, "Привет, с удовльствием скажу что у тебя по расписанию!", reply_markup=keyboard)
 
@@ -40,33 +40,113 @@ def answer(message):
         w = records[1][1]
         e = records[2][1]
         r = records[3][1]
-        print(q)
         if q == 'Нет':
-            q = "Первой пары у тебя нет!\n"
+            q = "1 - нет.\n"
         else:
-            q = ('Первая пара у тебя - ' + str(q) + '.\n')
+            q = ('1 - ' + str(q) + '.\n')
         if w == 'Нет':
-            w = "Второй пары у тебя нет!\n"
+            w = "2 - нет.\n"
         else:
-            w = ('Вторая пара у тебя - ' + str(w) + '.\n')
+            w = ('2 - ' + str(w) + '.\n')
         if e == 'Нет':
-            e = "Третьей пары у тебя нет!\n"
+            e = "3 - нет.\n"
         else:
-            e = ('Третья пара у тебя - ' + str(e) + '.\n')
+            e = ('3 - ' + str(e) + '.\n')
         if r == 'Нет':
-            r = "Четвертой пары у тебя нет!\n"
+            r = "4 - нет.\n"
         else:
-            r = ('Четвертая у тебя - ' + str(r) + '.\n')
-        t = str(q + w + e + r + 'Изи пизи!!')
+            r = ('4 - ' + str(r) + '.\n')
+        t = str('Расписание на сегодня: \n' + q + w + e + r)
+        bot.send_message(message.chat.id, t)
+
+    if message.text.lower() == 'завтра':
+        cursor.execute("SELECT num, lesson FROM Timetable WHERE date = (date(now()) + integer '1');")
+        records = list(cursor.fetchall())
+        q = records[0][1]
+        w = records[1][1]
+        e = records[2][1]
+        r = records[3][1]
+        if q == 'Нет':
+            q = "1 - нет.\n"
+        else:
+            q = ('1 - ' + str(q) + '.\n')
+        if w == 'Нет':
+            w = "2 - нет.\n"
+        else:
+            w = ('2 - ' + str(w) + '.\n')
+        if e == 'Нет':
+            e = "3 - нет.\n"
+        else:
+            e = ('3 - ' + str(e) + '.\n')
+        if r == 'Нет':
+            r = "4 - нет.\n"
+        else:
+            r = ('4 - ' + str(r) + '.\n')
+        t = str('Расписание на завтра: \n' + q + w + e + r)
+        bot.send_message(message.chat.id, t)
+        print(message.text)
+
+    if message.text.lower() == 'вчера':
+        cursor.execute("SELECT num, lesson FROM Timetable WHERE date = (date(now()) - integer '1');")
+        records = list(cursor.fetchall())
+        q = records[0][1]
+        w = records[1][1]
+        e = records[2][1]
+        r = records[3][1]
+        if q == 'Нет':
+            q = "1 - нет.\n"
+        else:
+            q = ('1 - ' + str(q) + '.\n')
+        if w == 'Нет':
+            w = "2 - нет.\n"
+        else:
+            w = ('2 - ' + str(w) + '.\n')
+        if e == 'Нет':
+            e = "3 - нет.\n"
+        else:
+            e = ('3 - ' + str(e) + '.\n')
+        if r == 'Нет':
+            r = "4 - нет.\n"
+        else:
+            r = ('4 - ' + str(r) + '.\n')
+        t = str('Вчерашнее расписание: \n' + q + w + e + r)
         bot.send_message(message.chat.id, t)
 
 
-       # bot.send_message(message.chat.id, records)
-        print(records)
-    elif message.text.lower() == 'не хочу' or message.text.lower() == 'нет':
-        bot.send_message(message.chat.id, ":(")
-    else:
-        bot.send_message(message.chat.id, "Не понял")
+
+    if message.text.lower() == 'по дате':
+        bot.send_message(message.chat.id, 'Введите число в формате ГГГГ-ММ-ДД \n Например: 2021-01-01')
+
+    if message.text.__contains__('2021'):
+        date1 = str(message.text)
+        cursor.execute("SELECT num, lesson FROM Timetable WHERE date = '{0}'".format(date1))
+        records = list(cursor.fetchall())
+        if records ==[]:
+            bot.send_message(message.chat.id, 'Пока расписания на это число нет, введите другую дату.')
+        else:
+            q = records[0][1]
+            w = records[1][1]
+            e = records[2][1]
+            r = records[3][1]
+            if q == 'Нет':
+                q = "1 - нет.\n"
+            else:
+                q = ('1 - ' + str(q) + '.\n')
+            if w == 'Нет':
+                w = "2 - нет.\n"
+            else:
+                w = ('2 - ' + str(w) + '.\n')
+            if e == 'Нет':
+                e = "3 - нет.\n"
+            else:
+                e = ('3 - ' + str(e) + '.\n')
+            if r == 'Нет':
+                r = "4 - нет.\n"
+            else:
+                r = ('4 - ' + str(r) + '.\n')
+            t = str('Расписание на ' + date1 + ':\n' + q + w + e + r)
+            bot.send_message(message.chat.id, t)
+
 
 
 
