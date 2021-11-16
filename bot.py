379ef824f1f1544548454e401a6a3de032fbd3c9
gosha <gosha1253@mail.ru>
2021-11-16 09:29:11 +0000

    1234

--- bot.py
+++ bot.py
@@ -1,5 +1,15 @@
 import telebot
+import psycopg2
 from telebot import TeleBot
+
+conn = psycopg2.connect(database="service",
+                        user="postgres",
+                        password="1234",
+                        host="localhost",
+                        port="5432")
+
+cursor = conn.cursor()
+
 
 token = '2135145672:AAGKUt03amS6MOrfcQpORDFRDWHwz4K073E'
 
@@ -9,23 +19,50 @@ bot: TeleBot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start(message):
     print(message.chat.id)
-    #starter = telebot.types.ReplyKeyboardMarkup()
     keyboard = telebot.types.ReplyKeyboardMarkup()
-    #starter.row('/start')
-    #bot.send_message(message.chat.id,' ', reply_markup=starter)
-    keyboard.row("Хочу", "Не хочу")
-    keyboard.row("Да", "Нет")
-    bot.send_message(message.chat.id, "Hello! Хочешь узнать что нибудь?", reply_markup=keyboard)
+    keyboard.row("Сейчас", "Сегодня")
+    keyboard.row("Завтра", "Вчера")
+    bot.send_message(message.chat.id, "Привет, с удовльствием скажу что у тебя по расписанию!", reply_markup=keyboard)
+
 
 @bot.message_handler(commands=["help"])
 def help(message):
     bot.send_message(message.chat.id, 'я умею....всё')
 
 
+
 @bot.message_handler(content_types=['text'])
 def answer(message):
-    if message.text.lower() == 'хочу'or message.text.lower() == 'да':
-        bot.send_message(message.chat.id, "Пиши тогда сюда: vk.com/gosha1253")
+    if message.text.lower() == 'сегодня':
+        cursor.execute("SELECT num, lesson FROM Timetable WHERE date = date(now());")
+        records = list(cursor.fetchall())
+        q = records[0][1]
+        w = records[1][1]
+        e = records[2][1]
+        r = records[3][1]
+        print(q)
+        if q == 'Нет':
+            q = "Первой пары у тебя нет!\n"
+        else:
+            q = ('Первая пара у тебя - ' + str(q) + '.\n')
+        if w == 'Нет':
+            w = "Второй пары у тебя нет!\n"
+        else:
+            w = ('Вторая пара у тебя - ' + str(w) + '.\n')
+        if e == 'Нет':
+            e = "Третьей пары у тебя нет!\n"
+        else:
+            e = ('Третья пара у тебя - ' + str(e) + '.\n')
+        if r == 'Нет':
+            r = "Четвертой пары у тебя нет!\n"
+        else:
+            r = ('Четвертая у тебя - ' + str(r) + '.\n')
+        t = str(q + w + e + r + 'Изи пизи!!')
+        bot.send_message(message.chat.id, t)
+
+
+       # bot.send_message(message.chat.id, records)
+        print(records)
     elif message.text.lower() == 'не хочу' or message.text.lower() == 'нет':
         bot.send_message(message.chat.id, ":(")
     else:
