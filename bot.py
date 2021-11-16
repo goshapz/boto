d39eb275ccf0ce17fc78b704735f48bb2adfc5a5
gosha <gosha1253@mail.ru>
2021-11-16 21:16:49 +0000

    анеки и учебники, будильник

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import telebot
 import psycopg2
 from telebot import TeleBot
+import random
+import schedule
+import datetime
+import time
 
 conn = psycopg2.connect(database="service",
                         user="postgres",
@@ -13,22 +17,30 @@ cursor = conn.cursor()
 
 token = '2135145672:AAGKUt03amS6MOrfcQpORDFRDWHwz4K073E'
 
+
 bot: TeleBot = telebot.TeleBot(token)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     print(message.chat.id)
-    keyboard = telebot.types.ReplyKeyboardMarkup()
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.row("По дате", "Сегодня")
     keyboard.row("Завтра", "Вчера")
+
     bot.send_message(message.chat.id, "Привет, с удовльствием скажу что у тебя по расписанию!", reply_markup=keyboard)
+    print(datetime.datetime.now().time())
 
 
-@bot.message_handler(commands=["help"])
-def help(message):
-    bot.send_message(message.chat.id, 'я умею....всё')
+def budilnik():
+    print("Спокойной ночи")
 
+    schedule.every(10).minutes.do(budilnik)
+    schedule.every().hour.do(budilnik)
+    schedule.every().monday.at("9:30").do(budilnik)
+    schedule.every().tuesday.at('9:30').do(budilnik)
+    schedule.every().wednesday.at("13:10").do(budilnik)
+    schedule.every().tuesday.at("21:30").do(budilnik)
 
 
 @bot.message_handler(content_types=['text'])
@@ -41,21 +53,21 @@ def answer(message):
         e = records[2][1]
         r = records[3][1]
         if q == 'Нет':
-            q = "1 - нет.\n"
+            q = "9:30-11:05 - нет.\n"
         else:
-            q = ('1 - ' + str(q) + '.\n')
+            q = ('9:30-11:05 - ' + str(q) + '.\n')
         if w == 'Нет':
-            w = "2 - нет.\n"
+            w = "11:20-12:55 - нет.\n"
         else:
-            w = ('2 - ' + str(w) + '.\n')
+            w = ('11:20-12:55 - ' + str(w) + '.\n')
         if e == 'Нет':
-            e = "3 - нет.\n"
+            e = "13:10-14:45 - нет.\n"
         else:
-            e = ('3 - ' + str(e) + '.\n')
+            e = ('13:10-14:45 - ' + str(e) + '.\n')
         if r == 'Нет':
-            r = "4 - нет.\n"
+            r = "15:25-17:00 - нет.\n"
         else:
-            r = ('4 - ' + str(r) + '.\n')
+            r = ('15:25-17:00 - ' + str(r) + '.\n')
         t = str('Расписание на сегодня: \n' + q + w + e + r)
         bot.send_message(message.chat.id, t)
 
@@ -67,21 +79,21 @@ def answer(message):
         e = records[2][1]
         r = records[3][1]
         if q == 'Нет':
-            q = "1 - нет.\n"
+            q = "9:30-11:05 - нет.\n"
         else:
-            q = ('1 - ' + str(q) + '.\n')
+            q = ('9:30-11:05 - ' + str(q) + '.\n')
         if w == 'Нет':
-            w = "2 - нет.\n"
+            w = "11:20-12:55 - нет.\n"
         else:
-            w = ('2 - ' + str(w) + '.\n')
+            w = ('11:20-12:55 - ' + str(w) + '.\n')
         if e == 'Нет':
-            e = "3 - нет.\n"
+            e = "13:10-14:45 - нет.\n"
         else:
-            e = ('3 - ' + str(e) + '.\n')
+            e = ('13:10-14:45 - ' + str(e) + '.\n')
         if r == 'Нет':
-            r = "4 - нет.\n"
+            r = "15:25-17:00 - нет.\n"
         else:
-            r = ('4 - ' + str(r) + '.\n')
+            r = ('15:25-17:00 - ' + str(r) + '.\n')
         t = str('Расписание на завтра: \n' + q + w + e + r)
         bot.send_message(message.chat.id, t)
         print(message.text)
@@ -94,25 +106,23 @@ def answer(message):
         e = records[2][1]
         r = records[3][1]
         if q == 'Нет':
-            q = "1 - нет.\n"
+            q = "9:30-11:05 - нет.\n"
         else:
-            q = ('1 - ' + str(q) + '.\n')
+            q = ('9:30-11:05 - ' + str(q) + '.\n')
         if w == 'Нет':
-            w = "2 - нет.\n"
+            w = "11:20-12:55 - нет.\n"
         else:
-            w = ('2 - ' + str(w) + '.\n')
+            w = ('11:20-12:55 - ' + str(w) + '.\n')
         if e == 'Нет':
-            e = "3 - нет.\n"
+            e = "13:10-14:45 - нет.\n"
         else:
-            e = ('3 - ' + str(e) + '.\n')
+            e = ('13:10-14:45 - ' + str(e) + '.\n')
         if r == 'Нет':
-            r = "4 - нет.\n"
+            r = "15:25-17:00 - нет.\n"
         else:
-            r = ('4 - ' + str(r) + '.\n')
+            r = ('15:25-17:00 - ' + str(r) + '.\n')
         t = str('Вчерашнее расписание: \n' + q + w + e + r)
         bot.send_message(message.chat.id, t)
-
-
 
     if message.text.lower() == 'по дате':
         bot.send_message(message.chat.id, 'Введите число в формате ГГГГ-ММ-ДД \n Например: 2021-01-01')
@@ -121,7 +131,7 @@ def answer(message):
         date1 = str(message.text)
         cursor.execute("SELECT num, lesson FROM Timetable WHERE date = '{0}'".format(date1))
         records = list(cursor.fetchall())
-        if records ==[]:
+        if not records:
             bot.send_message(message.chat.id, 'Пока расписания на это число нет, введите другую дату.')
         else:
             q = records[0][1]
@@ -129,28 +139,45 @@ def answer(message):
             e = records[2][1]
             r = records[3][1]
             if q == 'Нет':
-                q = "1 - нет.\n"
+                q = "9:30-11:05 - нет.\n"
             else:
-                q = ('1 - ' + str(q) + '.\n')
+                q = ('9:30-11:05 - ' + str(q) + '.\n')
             if w == 'Нет':
-                w = "2 - нет.\n"
+                w = "11:20-12:55 - нет.\n"
             else:
-                w = ('2 - ' + str(w) + '.\n')
+                w = ('11:20-12:55 - ' + str(w) + '.\n')
             if e == 'Нет':
-                e = "3 - нет.\n"
+                e = "13:10-14:45 - нет.\n"
             else:
-                e = ('3 - ' + str(e) + '.\n')
+                e = ('13:10-14:45 - ' + str(e) + '.\n')
             if r == 'Нет':
-                r = "4 - нет.\n"
+                r = "15:25-17:00 - нет.\n"
             else:
-                r = ('4 - ' + str(r) + '.\n')
+                r = ('15:25-17:00 - ' + str(r) + '.\n')
             t = str('Расписание на ' + date1 + ':\n' + q + w + e + r)
             bot.send_message(message.chat.id, t)
 
+    if message.text.lower() == '/анекдот':
+        a = random.randint(1, 10062)
+        cursor.execute("SELECT text FROM anek WHERE id = '{0}'".format(a))
+        records = list(cursor.fetchall())
+        bot.send_message(message.chat.id, records)
 
-
-
-
+    if message.text.lower() == '/агила':
+        bot.send_message(message.chat.id, 'https://vk.com/doc257116315_619803'
+                                         '977?hash=b5f4c0d3'
+                                          '7ede824274&dl=1ce00b0970a769ae03')
+    if message.text.lower() == '/математика':
+        bot.send_message(message.chat.id, 'https://vk.com/doc494484715_469501555?ha'
+                                          'sh=a49d028229b4105392&dl=d4cd6a1e1fa8faee13')
+    if message.text.lower() == '/английский язык':
+        bot.send_message(message.chat.id, 'https://vk.com/away.php?to=https%3A%2F'
+                                          '%2Fwww.book.ru%2Fbook%2F926036&cc_key=')
+    if message.text.lower() == '/help':
+        bot.send_message(message.chat.id, '/start \n /анекдот - Отправляет рандомный анекдот из огромной базы. '
+                                          '\n /агила - Учебник по алгебре.\n /математика - Берман сборник.'
+                                          ' \n  /английский язык - учебник по английскому.'
+                                          ' \n Команды будут добавляться :)')
 
 
 bot.infinity_polling()
