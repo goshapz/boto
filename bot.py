6194c83ac0fc52bc72c49c9504bbd952e3af774f
gosha <gosha1253@mail.ru>
2021-11-21 19:32:01 +0000

    отправка картинок

--- bot.py
+++ bot.py
@@ -18,19 +18,19 @@ token = '2135145672:AAGKUt03amS6MOrfcQpORDFRDWHwz4K073E'
 bot: TeleBot = telebot.TeleBot(token)
 
 
-@bot.message_handler(commands=['start', '/назад.....'])
+@bot.message_handler(commands=['start', 'назад.....'])
 def start(message):
-    print(message.chat.id)
     keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.row("По дате", "Сегодня")
     keyboard.row("Завтра", "Вчера")
-    keyboard.row('/анекдот', '/help')
-
-    bot.send_message(message.chat.id, "Привет, с удовльствием скажу что у тебя по расписанию!", reply_markup=keyboard)
+    keyboard.row('/anekdot', '/help')
+    bot.send_message(message.chat.id, 'Привет!\nИспользуй /help, '
+                                      'чтобы узнать список доступных команд! \nТак же можешь узнать рас'
+                                      'писание на интересующий день.', reply_markup=keyboard)
     print(datetime.datetime.now().time())
 
 
-@bot.message_handler(commands=['анекдот'])
+@bot.message_handler(commands=['anekdot'])
 def start(message):
     keyboard1 = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard1.row('ахАХАХахаХАХ11))0)')
@@ -45,6 +45,41 @@ def start(message):
     b = b.replace('[(\'', ' ')
     b = b.replace('\',)]', ' ')
     bot.send_message(message.chat.id, b, reply_markup=keyboard1)
+    print(datetime.datetime.now().time())
+    userf = message.from_user.first_name
+    users = message.from_user.last_name
+    username = (userf + ' ' + users)
+    print(username + ' ' + 'читает анекдот')
+    cursor.execute("SELECT * from telusers WHERE chat_id =%s", (str(message.chat.id),))
+    records = list(cursor.fetchall())
+    if not records:
+        cursor.execute('insert into telusers(chat_id, username) VALUES (%s, %s);',
+                       (str(message.chat.id), str(username)))
+        conn.commit()
+
+
+# тестовая функция
+@bot.message_handler(commands=['timetable'])
+def start(message):
+    # cursor.execute("SELECT * from category WHERE id_category = 10;")
+    # records = list(cursor.fetchall())
+    # a = records[0][2]
+    bot.send_photo(message.chat.id, open('/home/georgy/Документы/1.png', 'rb'))
+    bot.send_photo(message.chat.id, open('/home/georgy/Документы/2.png', 'rb'))
+    bot.send_photo(message.chat.id, open('/home/georgy/Документы/3.png', 'rb'))
+    bot.send_photo(message.chat.id, open('/home/georgy/Документы/4.png', 'rb'))
+    bot.send_photo(message.chat.id, open('/home/georgy/Документы/5.png', 'rb'))
+    print(datetime.datetime.now().time())
+    userf = message.from_user.first_name
+    users = message.from_user.last_name
+    username = (userf + ' ' + users)
+    print(username + ' ' + 'смотрит расписание')
+    cursor.execute("SELECT * from telusers WHERE chat_id =%s", (str(message.chat.id),))
+    records = list(cursor.fetchall())
+    if not records:
+        cursor.execute('insert into telusers(chat_id, username) VALUES (%s, %s);',
+                       (str(message.chat.id), str(username)))
+        conn.commit()
 
 
 ''' 
@@ -88,11 +123,21 @@ def answer(message):
             r = ('15:25-17:00 - ' + str(r) + '.\n')
         t = str('Расписание на сегодня: \n' + q + w + e + r)
         bot.send_message(message.chat.id, t)
+        print(datetime.datetime.now().time())
+        userf = message.from_user.first_name
+        users = message.from_user.last_name
+        username = (userf + ' ' + users)
+        print(username + ' ' + 'расписание на сегодня')
+        cursor.execute("SELECT * from telusers WHERE chat_id =%s", (str(message.chat.id),))
+        records = list(cursor.fetchall())
+        if not records:
+            cursor.execute('insert into telusers(chat_id, username) VALUES (%s, %s);',
+                           (str(message.chat.id), str(username)))
+            conn.commit()
 
     if message.text.lower() == 'завтра':
         cursor.execute("SELECT num, lesson FROM Timetable WHERE date = (date(now()) + integer '1');")
         records = list(cursor.fetchall())
-        print(records)
         q = records[0][1]
         w = records[1][1]
         e = records[2][1]
@@ -115,7 +160,17 @@ def answer(message):
             r = ('15:25-17:00 - ' + str(r) + '.\n')
         t = str('Расписание на завтра: \n' + q + w + e + r)
         bot.send_message(message.chat.id, t)
-        print(message.text)
+        print(datetime.datetime.now().time())
+        userf = message.from_user.first_name
+        users = message.from_user.last_name
+        username = (userf + ' ' + users)
+        print(username + ' ' + 'расписание на завтра')
+        cursor.execute("SELECT * from telusers WHERE chat_id =%s", (str(message.chat.id),))
+        records = list(cursor.fetchall())
+        if not records:
+            cursor.execute('insert into telusers(chat_id, username) VALUES (%s, %s);',
+                           (str(message.chat.id), str(username)))
+            conn.commit()
 
     if message.text.lower() == 'вчера':
         cursor.execute("SELECT num, lesson FROM Timetable WHERE date = (date(now()) - integer '1');")
@@ -142,6 +197,17 @@ def answer(message):
             r = ('15:25-17:00 - ' + str(r) + '.\n')
         t = str('Вчерашнее расписание: \n' + q + w + e + r)
         bot.send_message(message.chat.id, t)
+        print(datetime.datetime.now().time())
+        userf = message.from_user.first_name
+        users = message.from_user.last_name
+        username = (userf + ' ' + users)
+        print(username + ' ' + 'расписание на вчера')
+        cursor.execute("SELECT * from telusers WHERE chat_id =%s", (str(message.chat.id),))
+        records = list(cursor.fetchall())
+        if not records:
+            cursor.execute('insert into telusers(chat_id, username) VALUES (%s, %s);',
+                           (str(message.chat.id), str(username)))
+            conn.commit()
 
     if message.text.lower() == 'по дате':
         bot.send_message(message.chat.id, 'Введите число в формате ГГГГ-ММ-ДД \n Например: 2021-01-01')
@@ -175,6 +241,17 @@ def answer(message):
                 r = ('15:25-17:00 - ' + str(r) + '.\n')
             t = str('Расписание на ' + date1 + ':\n' + q + w + e + r)
             bot.send_message(message.chat.id, t)
+        print(datetime.datetime.now().time())
+        userf = message.from_user.first_name
+        users = message.from_user.last_name
+        username = (userf + ' ' + users)
+        print(username + ' ' + 'расписание по дате')
+        cursor.execute("SELECT * from telusers WHERE chat_id =%s", (str(message.chat.id),))
+        records = list(cursor.fetchall())
+        if not records:
+            cursor.execute('insert into telusers(chat_id, username) VALUES (%s, %s);',
+                           (str(message.chat.id), str(username)))
+            conn.commit()
 
     if message.text.lower() == 'ахахахахахах11))0)':
         a = random.randint(1, 10062)
@@ -187,22 +264,45 @@ def answer(message):
         b = b.replace('[(\'', ' ')
         b = b.replace('\',)]', ' ')
         bot.send_message(message.chat.id, b)
+        print(datetime.datetime.now().time())
+        userf = message.from_user.first_name
+        users = message.from_user.last_name
+        username = (userf + ' ' + users)
+        print(username + ' ' + 'читает анекдот')
+        cursor.execute("SELECT * from telusers WHERE chat_id =%s", (str(message.chat.id),))
+        records = list(cursor.fetchall())
+        if not records:
+            cursor.execute('insert into telusers(chat_id, username) VALUES (%s, %s);',
+                           (str(message.chat.id), str(username)))
+            conn.commit()
 
-    if message.text.lower() == '/агила':
+    if message.text.lower() == '/agila':
         bot.send_message(message.chat.id, 'https://vk.com/doc257116315_619803'
                                           '977?hash=b5f4c0d3'
                                           '7ede824274&dl=1ce00b0970a769ae03')
-    if message.text.lower() == '/математика':
+    if message.text.lower() == '/maths':
         bot.send_message(message.chat.id, 'https://vk.com/doc494484715_469501555?ha'
                                           'sh=a49d028229b4105392&dl=d4cd6a1e1fa8faee13')
-    if message.text.lower() == '/английский язык':
+    if message.text.lower() == '/english':
         bot.send_message(message.chat.id, 'https://vk.com/away.php?to=https%3A%2F'
                                           '%2Fwww.book.ru%2Fbook%2F926036&cc_key=')
     if message.text.lower() == '/help':
-        bot.send_message(message.chat.id, '/start \n /анекдот - Отправляет рандомный анекдот из огромной базы. '
-                                          '\n /агила - Учебник по алгебре.\n /математика - Берман сборник.'
-                                          ' \n  /английский язык - учебник по английскому.'
+        bot.send_message(message.chat.id, '/start \n  /anekdot - Отправляет рандомный анекдот из огромной базы. '
+                                          '\n  /agila - Учебник по алгебре.\n  /maths - Берман сборник.'
+                                          ' \n   /english - учебник по английскому.'
+                                          '\n  /timetable - расписание с сайта МТУСИ.'
                                           ' \n Команды будут добавляться :)')
+        print(datetime.datetime.now().time())
+        userf = message.from_user.first_name
+        users = message.from_user.last_name
+        username = (userf + ' ' + users)
+        print(username + ' ' + 'смотрит список команд')
+        cursor.execute("SELECT * from telusers WHERE chat_id =%s", (str(message.chat.id),))
+        records = list(cursor.fetchall())
+        if not records:
+            cursor.execute('insert into telusers(chat_id, username) VALUES (%s, %s);',
+                           (str(message.chat.id), str(username)))
+            conn.commit()
 
 
 bot.infinity_polling()
